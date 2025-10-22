cdcb0099db2d31458d0a67f9462b087307fa8ada
janham <janhampino@gmail.com>
2025-10-23 08:57:52 +0900

    add init file

---
+++ init.lua
@@ -0,0 +1,24 @@
+-- 基本設定の読み込み
+require("config.options")
+require("config.keymaps")
+
+-- lazy.nvimのインストールと設定
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- プラグインの読み込み
+require("lazy").setup("plugins", {
+  rocks = {
+    enabled = false
+  }
+})
